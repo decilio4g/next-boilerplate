c99514d6681dbf67175781ca5f8cb3984c0e156f
Decilio <gabremns@gmail.com>
2020-09-01 21:51:09 -0300

    api

---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b6155599-00f8-462e-a5cb-878d0fe6a963'
+};
---
+++ web/src/components/Main/index.tsx
@@ -0,0 +1,23 @@
+import * as S from "./styles";
+
+const Main = ({
+  title = "Next - Boilerplate",
+  description = "TypeScript, ReactJS, NextJS, Styled Components, Jest e Storybook",
+}) => {
+  return (
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela com código."
+      />
+    </S.Wrapper>
+  );
+};
+
+export default Main;
